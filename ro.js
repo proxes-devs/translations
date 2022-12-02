213f2ce20f67467ca885b13362c777dc258aaf0d
sixt <never.nightplay@gmail.com>
2022-12-03 00:42:56 +0200

    Romanian Translation

---
+++ ro.js
@@ -0,0 +1,53 @@
+'ro': {
+	// NAVBAR
+	':home': 'Acasă',
+	':forums': 'Forumuri',
+	':testimonials': 'Recenzii',
+	':contact': 'Contact',
+	':language': 'Limbă',
+	':other_projects': 'Alte Proiecte',
+	':contributors': 'Contribuitori',
+	// HEADER
+	':heading': 'Construiește-ți site-ul cu noi!',
+	':sub_heading': 'Creăm teme de forum și site-uri web pentru cele mai bune proiecte.',
+	':our_themes': 'Temele noastre',
+	':get_custom_theme': 'Obțineți o temă personalizată',
+	// FEATURES BLOCK
+	':fast_delivery': 'Livrare rapidă',
+	':fd_desc': 'În funcție de solicitarea temei, produsul este livrat în <strong>minim 3-4 zile</strong>.',
+	':quality_prod': 'Produs de calitate',
+	':qp_desc': 'Ne asigurăm că respectăm toate cerințele clienților pentru a crea cel mai bun produs.',
+	':low_prices': 'Prețuri mici',
+	':lp_desc': 'Costul unei teme personalizate începe de la 15 dolari, prețul fiind accesibil oricui.',
+	// COOPERATION BLOCK
+	':cooperation': 'Cum începe <span class="text-gradient">cooperarea</span>?',
+	':step_1': 'Etapa #1',
+	':step_2': 'Etapa #2',
+	':step_3': 'Etapa #3',
+	':step1_text': 'Primul contact',
+	':step2_text': 'Începe dezvoltarea',
+	':step3_text': 'Stadiu final',
+	':step1_desc': 'Clientul <a href="contact">ne contactează</a> și discutăm despre prețul și complexitatea proiectului.',
+	':step2_desc': "Echipa noastră începe să lucreze la produsul clientului.<br><em>Este nevoie de minim 3 zile pentru ca produsul să fie terminat.</em>",
+	':step3_desc': 'Clientul primeste produsul finit, apoi au loc formalitatile finale.',
+	// REVIEWS BLOCK
+	':reviews_heading': 'Recenziile clienților',
+	':reviews_subheading': 'Vezi ce cred clienții noștri despre noi.',
+	':more_reviews': 'Mai multe recenzii',
+	// FOOTER
+	':copyright': 'Drepturi rezervate © PROXES Community',
+	// CONTACT PAGE
+	':cont_heading': 'Contactaţi-ne!',
+	':cont_subheading': "Ne-am bucura sa primim vești de la dumneavoastră!",
+	':form_name': 'Nume',
+	':form_email': 'Email',
+	':form_message': 'Mesaj',
+	':form_submit': 'Trimite',
+	':thank_you_message': 'Email-ul a fost trimis cu succes! Mulțumim că ne-ai contactat.',
+	// CONTRIBUTORS PAGE
+	':contr_heading': 'Contribuitorii noștri',
+	':contr_subheading': 'Aceasta este o pagină specială dedicată persoanelor care ajută la construirea PROXES!',
+	// OTHER PROJECTS PAGE
+	':oproj_heading': 'Alte Proiecte',
+	':oproj_subheading': 'Vezi lista noastră de proiecte!'
+}
