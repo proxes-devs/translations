e26b87ce00a99abf3a002ec74d7fbd1c58029b2f
sixt <never.nightplay@gmail.com>
2022-12-02 18:47:20 +0200

    Add files via upload

---
+++ en.js
@@ -0,0 +1,53 @@
+'en': {
+	// NAVBAR
+	':home': 'Home',
+	':forums': 'Forums',
+	':testimonials': 'Testimonials',
+	':contact': 'Contact',
+	':language': 'Language',
+	':other_projects': 'Other Projects',
+	':contributors': 'Contributors',
+	// HEADER
+	':heading': 'Build your website with us!',
+	':sub_heading': 'Creating forum themes and websites for the best projects.',
+	':our_themes': 'Our themes',
+	':get_custom_theme': 'Get a Custom Theme',
+	// FEATURES BLOCK
+	':fast_delivery': 'Fast delivery',
+	':fd_desc': 'Depending on the theme request, the product gets delivered in <strong>minimum 3-4 days</strong>.',
+	':quality_prod': 'Quality product',
+	':qp_desc': 'We make sure to respect all the clients requirements in order to create the best product.',
+	':low_prices': 'Low prices',
+	':lp_desc': 'The cost of a custom theme starts at $15, the price being accessible to anyone.',
+	// COOPERATION BLOCK
+	':cooperation': 'How does the <span class="text-gradient">cooperation</span> begin?',
+	':step_1': 'Step #1',
+	':step_2': 'Step #2',
+	':step_3': 'Step #3',
+	':step1_text': 'First Contact',
+	':step2_text': 'Development begins',
+	':step3_text': 'Final stage',
+	':step1_desc': 'The client <a href="contact">contacts us</a> and we discuss about the price and complexity of the project.',
+	':step2_desc': "Our team starts working on the customer's product.<br><em>Takes a minimum of 3 days for the product to be finished.</em>",
+	':step3_desc': 'The client receives the finished product, then the final formalities occur.',
+	// REVIEWS BLOCK
+	':reviews_heading': 'Customer reviews',
+	':reviews_subheading': 'See what our customers think about us.',
+	':more_reviews': 'More Reviews',
+	// FOOTER
+	':copyright': 'Copyright © PROXES Community',
+	// CONTACT PAGE
+	':cont_heading': 'Contact us!',
+	':cont_subheading': "We'd love to hear from you!",
+	':form_name': 'Name',
+	':form_email': 'Email',
+	':form_message': 'Message',
+	':form_submit': 'Submit',
+	':thank_you_message': 'Email sent successfully! Thank you for contacting us.',
+	// CONTRIBUTORS PAGE
+	':contr_heading': 'Our contributors',
+	':contr_subheading': 'This is a special page dedicated to the people that help build PROXES!',
+	// OTHER PROJECTS PAGE
+	':oproj_heading': 'Other Projects',
+	':oproj_subheading': 'See our list of projects!'
+}
